b44d20d28c9cf9437434d953c4f3c29c8a29591a
Fernandot989 <110882090+Fernandot989@users.noreply.github.com>
2022-10-06 11:45:08 -0600

    Cambio

--- top.v
+++ top.v
@@ -2,10 +2,10 @@ module top (
 	input CLK,
 		input PIN_4, PIN_14, PIN_17, PIN_20, PIN_23, //Se establecen los pines de entrada
 		output PIN_7, PIN_8, PIN_9, PIN_10, PIN_11, PIN_12, PIN_13, //Se establecen los pines de salida
-    output LED,   // User/boot LED next to power LED
-    output USBPU  // USB pull-up resistor
+    output LED,   // Para darle poder a las LEDs
+    output USBPU  // Resistencia pull-up de la USB
 );	
-	// drive USB pull-up resistor to '0' to disable USB
+	// Se le establece el valor 0 para desabilitarlo
     assign USBPU = 1'b0;
 	
     wire siguiente, regresar, cancelar, enable, Y0, Y1, Y2, Y3, S2, S3, S4, reset, clk; //Se declara todas las variables como cables
@@ -28,10 +28,10 @@ module top (
     Ordenar FMS1(siguiente, regresar, cancelar, clk, reset, Y0, Y1); //Se llama a las maquinas con las variables que necesita cada variable.
 	Entregar FSM2(enable, siguiente, clk, reset, Y2, Y3);	
 	
-	// keep track of time and location in blink_pattern
+	// Mantener el tiempo y posición
     reg [25:0] counter;
 
-    // increment the blink_counter every clock
+    // Encrementar el tiempo cada cambio positivo del reloj
     always @(posedge CLK) begin
         counter <= counter + 1;
     end
